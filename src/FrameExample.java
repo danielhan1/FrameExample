5a9ab71b275955784e2c05175cb1503861532a32
Daniel <42978531+danielhan1@users.noreply.github.com>
2019-02-01 14:25:48 -0500

    Add files via upload

---
+++ src/FrameExample.java
@@ -0,0 +1,20 @@
+import java.awt.Point;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class FrameExample
+{
+		public static void main(String[] args)
+		{
+			JFrame myFrame = new JFrame();
+			myFrame.setBounds(300, 200, 500, 250);
+			myFrame.setLayout(null);
+			JLabel lbl1 = new JLabel("Welcome to GUI!");
+			lbl1.setBounds(35, 55, 200, 30);
+			myFrame.add(lbl1);
+			
+			myFrame.setVisible(true);
+			myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		}
+}
