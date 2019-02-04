645c3c9dcdae28a03e8d1cbfd041687c1d400557
Daniel <42978531+danielhan1@users.noreply.github.com>
2019-02-04 14:05:53 -0500

    Add files via upload

--- src/FrameExample.java
+++ src/FrameExample.java
@@ -2,7 +2,8 @@ import java.awt.Point;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
+import javax.swing.JButton;
+import java.awt.Dimension;
 public class FrameExample
 {
 		public static void main(String[] args)
@@ -13,8 +14,13 @@ public class FrameExample
 			JLabel lbl1 = new JLabel("Welcome to GUI!");
 			lbl1.setBounds(35, 55, 200, 30);
 			myFrame.add(lbl1);
-			lbl1.setFont()
+			JButton button1 = new JButton("My first button");
+			button1.setSize(new Dimension(100,30));
+			button1.setLocation(new Point(180, 30));
+			myFrame.add(button1);
+			//lbl1.setFont()
 			myFrame.setVisible(true);
 			myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}
 }
+   
